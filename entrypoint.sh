70964c9ef3a51bdf079d58b27d4d61166c3bb067
jockelmore <je.gaidusch@gmx.de>
2017-02-23 09:09:36 +0100

    Tried to fix bro bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,11 @@
 cd /logs
 touch temp.txt
 bro -i eth0 local init.bro &
+# Wait for bro to start
+until pids=$(pidof bro)
+do
+    sleep 1
+done
 cd /
 phantomjs netsniff.js $URL >> /logs/temp.txt
 cd logs
